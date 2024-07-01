d76b30f921dccd65623e5f8fa10d59513836b67a
Erick559 <erickungu82@gmail.com>
2024-07-01 16:06:54 +0300

    feat: Curate a list for all routes for the middleware.

---
+++ routes.ts
@@ -0,0 +1,32 @@
+/**
+ * An Array of routes that will only be accessible to the public not requiring authentication.
+ * These routes will be accessible by default by non-authorized users i.e the public.
+ * @type {string[]}
+ */
+export const publicRoutes = [
+    "/"
+]
+
+/**
+ * An Array of routes that represent the auth routes.
+ * These routes will be accessible by default to the public and will be used to authenticate users to the protected routes.
+ * @type {string[]}
+ */
+export const authRoutes = [
+    "/register",
+    "/login",
+]
+
+/**
+ * This is represents the prefix for the API authentication routes
+ * These routes are used for API authentication purposes and are to be public.
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth"
+
+
+/**
+ * After logging in or signing up this will be the default page the user will be redirected to.
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT_URL = "/dashboard"
