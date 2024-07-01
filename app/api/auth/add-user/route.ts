c0a89be2cdc1c535cbee0e5d242727da48630ad7
Erick559 <erickungu82@gmail.com>
2024-07-01 13:53:24 +0300

    fix: Pass the status code the right way.

--- app/api/auth/add-user/route.ts
+++ app/api/auth/add-user/route.ts
@@ -53,7 +53,7 @@ export async function POST(req:Request){
             return NextResponse.json({ message: errorMessages},{status: 400});
         }
         console.log(error);
-        return NextResponse.json({message: "Something went wrong!", status:500})
+        return NextResponse.json({message: "Something went wrong!"},{status:500})
     }
 
 }
