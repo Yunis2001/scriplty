1f94f78b750bbbd702525deb507500ed3a90f98a
Erick559 <erickungu82@gmail.com>
2024-07-02 10:51:42 +0300

    feat: Add authorization function for credentials logins. TODO: Will fix the signInCredentials error.

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,45 @@
-import GitHub  from "next-auth/providers/github";
 import { NextAuthConfig } from "next-auth";
+import Credentials  from "next-auth/providers/credentials"
+import bycrypt from "bcryptjs";
+
+import { db } from "./lib/prisma";
+import { LoginFormSchema } from '@/schemas';
+
 
 export default {
-    providers: [GitHub]
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const validatedFields = LoginFormSchema.safeParse(credentials);
+
+                if(validatedFields.success){
+                    const { email , password} = validatedFields.data;
+
+                    const user = await db.user.findUnique({
+                        where: {
+                            email:email
+                        }
+                    })
+
+                    if(!user || !user.password) {
+                        return null;
+                    }
+                    
+                    const passWordMatch = await bycrypt.compare(password,user.password);
+
+                    if (!passWordMatch) {
+                        return null;
+                    }
+
+                    return {
+                        id:`${user.id}`,
+                        name:user.name,
+                        email:user.email,                        
+                    };
+                }
+
+                return null;
+            }
+        })
+    ]
 } satisfies NextAuthConfig
