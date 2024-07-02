4cccc811baaa19f6c44610b68fd91d3272d21fb5
Erick559 <erickungu82@gmail.com>
2024-07-02 14:32:01 +0300

    feat: Expand the session object to include the user id.

--- auth.ts
+++ auth.ts
@@ -5,6 +5,17 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./lib/prisma"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks:{
+    async session({session,token}) {
+      if(token.sub && session.user){
+        session.user.id = token.sub;
+      }
+      return session
+    },
+    async jwt({token}) {
+      return token;
+    }
+  },
   adapter:PrismaAdapter(db),
   session:{strategy:'jwt'},
   ...authConfig,
