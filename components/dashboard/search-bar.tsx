26359fa48a9cc03535749a0752b1ab88499aa570
Erick559 <erickungu82@gmail.com>
2024-07-04 19:02:20 +0300

    fix: Customize the cursor to a pointer.

--- components/dashboard/search-bar.tsx
+++ components/dashboard/search-bar.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { Search } from "lucide-react";
-import { Button } from "../ui/button";
 
 const SearchBar = () => {
     const onClick = () => {
@@ -11,7 +10,7 @@ const SearchBar = () => {
         <div>
             <form className="w-full relative lg:flex gap-5 items-center" action="">
                 <input className="w-full py-2 px-3 rounded-2xl border-solid border-[1.5px] border-black flex-1" type="text" placeholder="Search a document" />
-                <Search className="absolute top-1/4 right-3 lg:relative lg:w-8 lg:h-8 stroke-[1px]" onClick={()=>onClick} />
+                <Search className="cursor-pointer absolute top-1/4 right-3 lg:relative lg:w-8 lg:h-8 stroke-[1px]" onClick={onClick} />
             </form>
         </div>
      );
