c0d8b8029624c5cb8b2f394a7207eb66f2a95016
Erick559 <erickungu82@gmail.com>
2024-07-06 05:51:36 +0300

    feat: Add a boolean prop to ensure the upload component is displayed in the dashboard page only.d

--- components/dashboard/document-grid.tsx
+++ components/dashboard/document-grid.tsx
@@ -3,27 +3,18 @@
 import { Plus, Trash } from "lucide-react"
 import { Button } from "../ui/button"
 import Link from "next/link"
+import UploadFile from "../upload-file"
 
-function DocumentsGrid() {
+interface DocumentsGridProps {
+    showUploadComponent?: boolean
+}
+
+function DocumentsGrid({showUploadComponent}:DocumentsGridProps) {
   return (
     <div className="w-full my-10 pb-5 grid justify-items-center sm:justify-items-start grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-3 gap-y-9">
+        {showUploadComponent && <UploadFile />}
         <article className="w-[300px] sm:w-[200px] bg-white flex flex-col gap-4">
-            <Link className="h-full" href='/'>
-                <header className="w-full text-center shadow-xl py-4 px-5 rounded-md border-2 outline-none h-full flex items-center justify-center">
-                    <Button variant='outline' size='lg' className="rounded-full w-[100px] h-[100px]">
-                        <Plus className="w-10 h-10" />
-                    </Button>
-                </header>
-            </Link>
-            <footer className="w-full flex justify-center shadow-2xl py-1 rounded-md border-2 outline-none">
-                <Button className="w-full h-full" variant='ghost' onClick={()=> {}}>
-                    Upload Document
-                </Button>
-            </footer>
-        </article>
-
-        <article className="w-[300px] sm:w-[200px] bg-white flex flex-col gap-4">
-            <Link href='/'>
+            <Link href='/docs/123'>
                 <header className="w-full shadow-xl py-4 px-5 rounded-md border-2 outline-none">
                     <h1 className="font-bold uppercase mb-3 text-lg">Document Title</h1>
                     <p className="text-sm sm:text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sit cumque soluta aspernatur inventore quisquam ipsam fuga, iure, expedita sunt mollitia vel officia ducimus, molestias accusantium doloremque adipisci sint facilis.</p>
