582160a5f48354c675036e5f37bc02542bc4e5d0
Erick559 <erickungu82@gmail.com>
2024-07-06 17:54:08 +0300

    feat: Create a logout button component that will be reused on all components that need signing out.

---
+++ components/logout-button.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { logOut } from "@/app/actions/Logout"
+import { LogOut } from "lucide-react";
+import { Button } from "./ui/button";
+
+interface LogoutButtonProps {
+    className?: string;
+}
+
+const LogoutButton = ({className}:LogoutButtonProps) => {
+    const onClick = ()=> {
+        logOut();
+    }
+
+  return (
+    <div className={`flex items-center gap-3 ${className?className:""}`}>
+        <LogOut className='h-7 w-7 stroke-[1px]' />
+        <Button onClick={onClick} variant='link' className={`text-2xl font-[400] ${className?className:''}`}>Sign Out</Button>
+    </div>
+  )
+}
+
+export default LogoutButton
