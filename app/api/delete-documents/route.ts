38e4823aad012f394751bd5c296e3e105f578aed
Erick559 <erickungu82@gmail.com>
2024-07-06 14:33:22 +0300

    feat: Create a delete documents endpoint.

---
+++ app/api/delete-documents/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function DELETE(req:Request) {
+    try {
+        const documentId = await req.json();
+
+        if(!documentId || documentId === undefined) {
+            return NextResponse.json({message: 'Document not found',status: 404})
+        }
+
+        await db.originalDocument.delete({
+            where: {
+                document_id:documentId,
+            }
+        })
+
+        return NextResponse.json({message: documentId})
+    } catch (error) {
+        return NextResponse.json(error);
+    }
+} 
