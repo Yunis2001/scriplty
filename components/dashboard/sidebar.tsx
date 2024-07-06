fe4907934a7760a665dbac9442059314b1729a22
Erick559 <erickungu82@gmail.com>
2024-07-06 18:27:51 +0300

    fix: Remove the unused logout function and other imports.

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -1,15 +1,10 @@
 'use client'
 
-import { Home, LogOut, Trash } from "lucide-react"
+import { Home,} from "lucide-react"
 import Link from "next/link"
-import { logOut } from "@/app/actions/Logout"
 import LogoutButton from "../logout-button"
 
 function Sidebar() {
-    const onClick = ()=>{
-        logOut();
-    }
-
   return (
     <div className="w-[300px] hidden px-10 pt-44 pb-32 lg:flex flex-col justify-between min-h-screen bg-blue-700">
         <div>
