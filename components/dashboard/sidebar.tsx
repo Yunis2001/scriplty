e4315fca8e6a33c5c296b43c56850065f829cb82
Erick559 <erickungu82@gmail.com>
2024-07-06 17:54:35 +0300

    fix: Modifiy the needed components with the new logout button.

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -3,6 +3,7 @@
 import { Home, LogOut, Trash } from "lucide-react"
 import Link from "next/link"
 import { logOut } from "@/app/actions/Logout"
+import LogoutButton from "../logout-button"
 
 function Sidebar() {
     const onClick = ()=>{
@@ -12,16 +13,11 @@ function Sidebar() {
   return (
     <div className="w-[300px] hidden px-10 pt-44 pb-32 lg:flex flex-col justify-between min-h-screen bg-blue-700">
         <div>
-            <Link className="flex gap-2 items-center text-white" href="/dashboard">
-                <Home className="w-7 h-7 stroke-[1px]" /> <span className="text-xl">Home</span>
+            <Link className="flex gap-3 items-center text-white" href="/dashboard">
+                <Home className="w-7 h-7 stroke-[1px]" /> <span className="text-2xl">Home</span>
             </Link>
         </div>
-
-        <div>
-            <button onClick={onClick} className="text-white flex gap-2 items-center text-left justify-start w-full font-extrabold">
-                <LogOut className="w-7 h-7 stroke-[1px]"/> <span className="text-xl">Sign Out</span>
-            </button>
-        </div>
+        <LogoutButton className="text-white" />
     </div>
   )
 }
