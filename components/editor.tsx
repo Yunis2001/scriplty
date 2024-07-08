e524b4fd63e76811e989002f5754cd01cd7f232a
Erick559 <erickungu82@gmail.com>
2024-07-08 22:37:31 +0300

    feat: Create the text editor component using tiptap. The editor is passed a parameter text that is the extracted text from the docx file.

---
+++ components/editor.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+
+interface EditorProps {
+    content:string
+    editable?:boolean | true,
+}
+
+const Editor = ({content,editable=false}:EditorProps) => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: content,
+    editable:editable
+  })
+
+  return <EditorContent editor={editor} />
+}
+
+export default Editor
