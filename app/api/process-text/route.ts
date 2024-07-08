7c0d697d497bbdaf03ed11057db3c7d1b68c48bf
Erick559 <erickungu82@gmail.com>
2024-07-09 00:01:13 +0300

    feat: Create a route for processing the text and gathering grammar suggestions.

---
+++ app/api/process-text/route.ts
@@ -0,0 +1,17 @@
+import { processText } from "@/lib/process-text";
+import { NextResponse } from "next/server";
+
+export async function POST(req:Request) {
+    try {
+        const body = await req.json();
+
+        const {text} = body;
+
+        const result = await processText(text);
+
+        return NextResponse.json({message:'ok',result:result})
+    }
+    catch(error){
+        return NextResponse.json({message:'error',error})
+    }
+}
