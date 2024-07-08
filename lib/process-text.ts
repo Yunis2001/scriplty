d5d62e2349fb2c29cc0840d2c3eceb01678112e9
Erick559 <erickungu82@gmail.com>
2024-07-08 07:55:53 +0300

    feat: Create the process-text function that accepts the raw text and outputs suggestions for the text.

---
+++ lib/process-text.ts
@@ -0,0 +1,33 @@
+import {retext} from 'retext';
+import retextEnglish from 'retext-english';
+import retextSyntaxUrls from 'retext-syntax-urls';
+import retextSpell from 'retext-spell';
+import retextRepeatedWords from 'retext-repeated-words';
+import retextIndefiniteArticle from 'retext-indefinite-article';
+import retextSentenceSpacing from 'retext-sentence-spacing';
+import retextQuotes from 'retext-quotes';
+import retextSimplify from 'retext-simplify';
+
+
+export const processText = async(text:string) => {
+    const processor = retext()
+        .use(retextEnglish)
+        .use(retextSimplify)
+        .use(retextSyntaxUrls)
+        .use(retextSentenceSpacing)
+        .use(retextIndefiniteArticle)
+        .use(retextQuotes,{preferred:'straight'})
+        .use(retextRepeatedWords);
+
+    const processedText = await processor.process(text);
+
+    const suggestions = processedText.messages.map((message)=> ({
+        type: message.source,
+        index:message.place,
+        original:message.actual,
+        suggestion:message.expected ? message.expected[0] : null,
+        message:message.reason,
+    }))
+
+    console.log(suggestions);
+}
