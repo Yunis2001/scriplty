61fbb124161939f90e1bc11c0ec66818f8333a45
Erick559 <erickungu82@gmail.com>
2024-07-09 08:09:42 +0300

    fix: Fix the body declaration as previously it was returning undefined.

--- app/api/process-text/route.ts
+++ app/api/process-text/route.ts
@@ -4,12 +4,11 @@ import { NextResponse } from "next/server";
 export async function POST(req:Request) {
     try {
         const body = await req.json();
-
-        const {text} = body;
+        const text = body
 
         const result = await processText(text);
 
-        return NextResponse.json({message:'ok',result:result})
+        return NextResponse.json({message:'ok',result,status:200})
     }
     catch(error){
         return NextResponse.json({message:'error',error})
