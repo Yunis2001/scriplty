827235d430a25c2754bd37e69aaa19757961ccda
Erick559 <erickungu82@gmail.com>
2024-07-10 11:41:57 +0300

    feat: Add download route.

---
+++ app/api/document-download/route.ts
@@ -0,0 +1,45 @@
+import { db } from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+import htmlToDocx from 'html-to-docx';
+import showdown from 'showdown';
+
+export async function POST(req:Request) {
+    try {
+        const body = await req.json();
+        const documentId = body.document_id;
+        const converter = new showdown.Converter();
+
+        const document  = await db.document.findUnique({
+            where: {
+                document_id:documentId,
+            },
+            select: {
+                processedText:true,
+                title:true,
+            }
+        })
+
+        if (!document) {
+            return NextResponse.json({ message: 'Document not found', status:404})
+        }
+
+        const processedHTMLText = converter.makeHtml(document.processedText);
+
+        const buffer = await htmlToDocx(processedHTMLText,'',{
+            table: { row: { cantSplit: true } },
+            footer: true,
+            pageNumber: true,
+        },'');
+
+        const response = new Response(buffer);
+
+        // Set the appropriate headers for file download
+        response.headers.set('Content-Disposition', `attachment; filename=${document.title}.docx`)
+        response.headers.set('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+
+        return response;
+    } catch (error) {
+        console.error('Error converting to docx:', error)
+        return NextResponse.json({ message: 'Internal Server Error' ,status:500})
+    }
+}
